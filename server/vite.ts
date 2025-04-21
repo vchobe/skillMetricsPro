b25f6d83aec5f9044761aa3e2470e6a3b22e719d
vchobe <vinayak.chobe@gmail.com>
2025-04-21 18:21:24 +0530

    Revert "vite gm2"
    This reverts commit ec6db9e87a07314a7768e63eb933b939c32a3386.

--- server/vite.ts
+++ server/vite.ts
@@ -4,7 +4,6 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { createServer as createViteServer, createLogger } from "vite";
 const __filename = fileURLToPath(import.meta.url);
-import { log } from "./utils";
 const __dirname = dirname(__filename);
 import { type Server } from "http";
 import viteConfig from "../vite.config";
@@ -12,6 +11,16 @@ import { nanoid } from "nanoid";
 
 const viteLogger = createLogger();
 
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  console.log(`${formattedTime} [${source}] ${message}`);
+}
 
 export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
